225751ecde190e59b58052a7c8390005192778de
Vincent-Murienne <muriennevincent@gmail.com>
2022-12-02 14:30:54 +0100

    <dev>(Form) : Création des pages

---
+++ GSB - gesAMM/GSB - gesAMM/frmNewMedicament.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace GSB___gesAMM
+{
+    partial class frmNewMedicament
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmNewMedicament
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "frmNewMedicament";
+            this.Text = "frmNewMedicament";
+            this.Load += new System.EventHandler(this.frmNewMedicament_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ GSB - gesAMM/GSB - gesAMM/frmNewMedicament.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace GSB___gesAMM
+{
+    public partial class frmNewMedicament : Form
+    {
+        public frmNewMedicament()
+        {
+            InitializeComponent();
+        }
+
+        private void frmNewMedicament_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
