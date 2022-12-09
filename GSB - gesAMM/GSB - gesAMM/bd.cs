57d894c68e4a7342c4fef4165229a57cf0033fa7
Vincent-Murienne <muriennevincent@gmail.com>
2022-12-09 14:20:33 +0100

    <Feat>(sqlServer): Connexion terminé

--- GSB - gesAMM/GSB - gesAMM/bd.cs
+++ GSB - gesAMM/GSB - gesAMM/bd.cs
@@ -9,5 +9,27 @@ namespace GSB___gesAMM
 {
     internal class bd
     {
+        /*public static void lireLesMedicamentsFamilles()
+        {
+            globale.lesClients.Clear();
+
+            //objet SQLCommand pour définir la procédure stockée à utiliser
+            SqlCommand maRequete = new SqlCommand("prc_med_famille", globale.cnx);
+            maRequete.CommandType = System.Data.CommandType.StoredProcedure;
+
+            // exécuter la procedure stockée dans un curseur 
+            SqlDataReader SqlExec = maRequete.ExecuteReader();
+
+            //boucle de lecture des clients avec ajout dans la collection
+            while (SqlExec.Read())
+            {
+                int idClient = int.Parse(SqlExec["idClient"].ToString());
+                string nomClient = SqlExec["nomClient"].ToString();
+
+                client leClient = new client(idClient, nomClient);
+
+                globale.lesClients.Add(leClient);
+            }
+        }*/
     }
 }
